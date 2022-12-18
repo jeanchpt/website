6341675a48c76c6d5d617c3186fcc9647594d112
Jean Chaput <jean.chaput@protonmail.com>
2022-12-18 23:04:16 +0100

    init: project init

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Jean Chaput';
+export const SITE_DESCRIPTION = 'Mon site web et blog personnel !';
